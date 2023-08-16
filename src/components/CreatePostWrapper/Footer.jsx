ebc77e1eeeb9b7c6a6e34bde93313627b1687303
Shuja-Siddiqui <140815099+s-shuja-rehman@users.noreply.github.com>
2023-08-16 21:14:30 +0500

    preview image and video enable

--- src/components/CreatePostWrapper/Footer.jsx
+++ src/components/CreatePostWrapper/Footer.jsx
@@ -1,28 +1,69 @@
-import './style.css';
+import { useState, useEffect } from "react";
+import "./style.css";
+
 export const Footer = () => {
+  const [values, setValues] = useState(null);
+  const [name, setName] = useState("");
+  
+  useEffect(() => {
+    if (name === "image") {
+      document.getElementById("pic2").style.display = "block";
+      document.getElementById("preview-video").style.display = "none";
+    }
+    if (name === "video") {
+      document.getElementById("pic2").style.display = "none";
+      document.getElementById("preview-video").style.display = "block";
+    }
+  }, [name]);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setValues(URL.createObjectURL(file).replace(/^blob:/, ""));
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const fileType = e.target.result.split(";")[0].split("/")[0].split(":")[1];
+        setName(fileType);
+        
+        if (fileType === "image") {
+          document.getElementById("pic2").src = e.target.result;
+        }
+        if (fileType === "video") {
+          document.getElementById("preview-video").src = e.target.result;
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <footer className='footer border-0 fixed'>
-      <div className='container'>
-        <ul className='element-list'>
+    <footer className="footer border-0 fixed">
+      <div className="container" style={name==="video"?{position:"relative"}:{}}>
+        <ul className="element-list">
           <li>
-            <input name='file' id='file-input' type='file' />
-            <label for='file-input'>
-              <i className='fa-solid fa-file-image'></i>Photo/Video
+            <input
+              name="file"
+              id="file-input"
+              type="file"
+              accept="image/*, video/*"
+              onChange={handleImageUpload}
+            />
+            <label htmlFor="file-input">
+              <i className="fa-solid fa-file-image"></i> Photo/Video
             </label>
-            <div id='preview-container' style={{ display: 'none' }}>
+            <div className="media media-50" style={name !== "" ? { width: "320px", height: "auto", marginTop:"20px" } : {}}>
               <img
-                id='preview-image'
-                style={{ display: 'none' }}
-                alt='image1'
+                id="pic2"
+                src=""
+                alt="profile"
+                style={name === "image" ? { display: "block" } : { display: "none" }}
               />
               <video
-                id='preview-video'
-                style={{ display: 'none' }}
+                id="preview-video"
+                style={name === "video" ? { display: "block", marginLeft:"20px" } : { display: "none" }}
                 controls
               ></video>
             </div>
-
-            {/* <!-- Rest of your HTML code --> */}
           </li>
         </ul>
       </div>
