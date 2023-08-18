0ba4270c01b2de7197fa46cdba8bca00e0171183
HassaanMujtaba <saddiquehari2511@gmail.com>
2023-08-18 11:08:57 -0700

    create post completed

--- src/components/CreatePostWrapper/CreatePost.jsx
+++ src/components/CreatePostWrapper/CreatePost.jsx
@@ -1,20 +1,26 @@
+import { useEffect, useState } from "react";
+import { getSingleUser } from "../../api";
+import config from "../../api/config";
 
 export const CreatePost = ({setText}) => {
-   
+   const [userName , setUserName]=useState('');
    const handleTextChange = (e) => {
     setText(e.target.value);  };
-  
+     useEffect(()=>{
+      getSingleUser().then((res)=> setUserName(res)).catch((err)=>console.log(err))
+     },[])
+      
   return (
     <div className='page-content'>
       <div className='container'>
         <div className='post-profile'>
           <div className='left-content'>
             <div className='media media-50 rounded-circle'>
-              <img id='pic' src='' alt='profile' />
+              <img id='pic' src={userName?.profileImage} alt='profile' />
             </div>
             <div className='ms-2'>
               <h6 id='name' className='mb-1'>
-                Abdul Raheem
+                {userName?.firstName + " " + userName?.lastName}
               </h6>
             </div>
           </div>
