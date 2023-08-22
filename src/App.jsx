d12126da4f7693648d517b0208ba3c8552e2e242
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-08-22 19:56:53 +0500

    Fixed ui bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import {
   Chat,
   Comment,
@@ -11,25 +11,33 @@ import {
   Profile,
   Setting,
   Timeline,
-} from "./pages";
-import { About, Forget, Login, PrivacyPolicy, Signup, Terms } from "./components";
-import { signup, login } from "../src/api/auth";
-import { currentLocation, generateRandomPassword } from "../src/utils";
+} from './pages';
+import {
+  About,
+  Forget,
+  Login,
+  PrivacyPolicy,
+  Signup,
+  Terms,
+} from './components';
+import { signup, login } from '../src/api/auth';
+import { currentLocation, generateRandomPassword } from '../src/utils';
 
 export default function App() {
-  const [islogin, setIsLogin] = useState(localStorage.getItem("@twinphy-token") || null);
+  const [islogin, setIsLogin] = useState(
+    localStorage.getItem('@twinphy-token') || null
+  );
   const createUser = async () => {
     const userDataCookie = document.cookie
-      .split(";")
-      .find((cookie) => cookie.trim().startsWith("userData="));
+      .split(';')
+      .find((cookie) => cookie.trim().startsWith('userData='));
 
     if (userDataCookie) {
-      const cookieName = userDataCookie.split("=")[0].trim();
-      const decodedData = decodeURIComponent(userDataCookie.split("=")[1]);
+      const cookieName = userDataCookie.split('=')[0].trim();
+      const decodedData = decodeURIComponent(userDataCookie.split('=')[1]);
       const userData = JSON.parse(decodedData);
       currentLocation()
         .then((res) => {
-         
           const randomPassword = generateRandomPassword();
           signup({
             firstName: userData?.name?.givenName,
@@ -38,41 +46,39 @@ export default function App() {
             email: userData?.emails[0].value,
             password: randomPassword,
             avatar: userData?.photos[0].value,
-            gender: "male",
-            dateOfBirth: "1990-01-01",
+            gender: 'male',
+            dateOfBirth: '1990-01-01',
             city: res.city,
             country: res.country,
           })
             .then((res) => {
-              
               if (res.response) {
                 if (
                   res.response?.data?.message ===
-                  "User with same email already exist"
+                  'User with same email already exist'
                 ) {
-                  
                   login({
                     email: userData?.emails[0].value,
-                    login_type: "google",
+                    login_type: 'google',
                   })
                     .then((res) => {
-                      localStorage.setItem("@twinphy-token", res?.data?.token);
+                      localStorage.setItem('@twinphy-token', res?.data?.token);
                       localStorage.setItem(
-                        "@twinphy-user",
+                        '@twinphy-user',
                         JSON.stringify(res?.data?.user)
                       );
                     })
                     .catch((err) => console.log(err));
                 }
               }
-              localStorage.setItem("@twinphy-token", res?.data?.token);
+              localStorage.setItem('@twinphy-token', res?.data?.token);
               localStorage.setItem(
-                "@twinphy-user",
+                '@twinphy-user',
                 JSON.stringify(res?.data?.user)
               );
             })
             .catch((err) => {
-              console.log(err, "err");
+              console.log(err, 'err');
             });
         })
         .catch((err) => console.log(err));
@@ -86,37 +92,40 @@ export default function App() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (localStorage.getItem("@twinphy-token") === null || localStorage.getItem("@twinphy-token") === "undefined") {
+    if (
+      localStorage.getItem('@twinphy-token') === null ||
+      localStorage.getItem('@twinphy-token') === 'undefined'
+    ) {
       setIsLogin(false);
-      navigate("/login");
+      navigate('/login');
     } else {
       setIsLogin(true);
     }
   }, [localStorage]);
-  
+
   return islogin ? (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/timeline" element={<Timeline />} />
-      <Route path="/explore" element={<Explore />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/message/:id" element={<Message />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/notifications" element={<Notification />} />
-      <Route path="/comment" element={<Comment />} />
-      <Route path="/create-post" element={<CreatePost />} />
+      <Route path='/' element={<HomePage />} />
+      <Route path='/home' element={<HomePage />} />
+      <Route path='/timeline' element={<Timeline />} />
+      <Route path='/explore' element={<Explore />} />
+      <Route path='/chat' element={<Chat />} />
+      <Route path='/message/:id' element={<Message />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/notifications' element={<Notification />} />
+      <Route path='/comment' element={<Comment />} />
+      <Route path='/create-post' element={<CreatePost />} />
+      <Route path='/settings' element={<Setting />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+      <Route path='/terms' element={<Terms />} />
     </Routes>
   ) : (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/forget" element={<Forget />} />
-      <Route path="/settings" element={<Setting />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      <Route path="/terms" element={<Terms />} />
+      <Route path='/' element={<Login />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/forget' element={<Forget />} />
     </Routes>
   );
 }
